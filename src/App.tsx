5c9e414c097951cc88e94ecd69b6c67e2c0f18fd
CodeSandbox <support@codesandbox.io>
2023-09-26 17:58:00 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <ThemeProvider>
       <ShellBar primaryTitle="UI5 Web Components for React Developer Challenge" />
       {/* Add your name and change title based on Week */}
-      <ColoredTitle title="<Insert Name Here> Week 1 - Tables" />
+      <ColoredTitle title="<Jeroen Roels> Week 1 - Tables" />
       <Solution />
     </ThemeProvider>
   );
