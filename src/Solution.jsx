b63116a9437da42c012860d5fb00d8bb70708dab
Jeroen Roels <roels_jeroen@hotmail.com>
2023-09-27 07:29:45 +0000

    Week 2: Custom component + extra

--- src/Solution.jsx
+++ src/Solution.jsx
@@ -1,5 +1,6 @@
-import { Label } from "@ui5/webcomponents-react";
+import { Badge, Label } from "@ui5/webcomponents-react";
 import { Table, TableRow, TableColumn, TableCell } from "@ui5/webcomponents-react";
+import { AnalyticalTable } from "@ui5/webcomponents-react";
 
 const tableData = [
   {
@@ -23,6 +24,18 @@ const tableData = [
     status: "Blocked",
   },
 ];
+
+const formatStatusCode = (status) => {
+  if (status === "Completed") {
+    return 8;
+  } else if (status === "In Progress") {
+    return 1;
+  } else if (status === "Blocked") {
+    return 3;
+  } else {
+    return 10;
+  }
+};
 
 function Solution() {
   return (
@@ -48,24 +61,71 @@ function Solution() {
           tableData.length && tableData.map(tableLine =>
           (<TableRow key={tableLine.id}>
             <TableCell>
-              <Label>
-                {tableLine.id}
-              </Label>
+              <Label>{tableLine.id}</Label>
             </TableCell>
             <TableCell>
-              <Label>
-                {tableLine.title}
-              </Label>
+              <Label>{tableLine.title}</Label>
             </TableCell>
             <TableCell>
-              <Label>
-                {tableLine.status}
-              </Label>
+              <Badge colorScheme={formatStatusCode(tableLine.status)}>{tableLine.status}</Badge>       
             </TableCell>
           </TableRow>)
           )
         }
       </Table>
+
+
+      <AnalyticalTable
+        columns={[
+          {
+            Header: 'ID',
+            accessor: 'id',
+            headerTooltip: 'ID'
+          },
+          {
+            Header: 'Title',
+            accessor: 'title',
+            headerTooltip: 'Title'
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+            Cell: (instance) => (<Badge colorScheme={formatStatusCode(instance.value)}>{instance.value}</Badge>)
+          },
+          {
+            Cell: function ka() { },
+            Header: 'Actions',
+            accessor: '.',
+            disableFilters: true,
+            disableGroupBy: true,
+            disableResizing: true,
+            disableSortBy: true,
+            id: 'actions',
+            minWidth: 100,
+            width: 100
+          }
+        ]}
+        data={tableData}
+        filterable
+        groupBy={[]}
+        groupable
+        header="Table Title"
+        infiniteScroll
+        onColumnsReorder={function ka() { }}
+        onGroup={function ka() { }}
+        onLoadMore={function ka() { }}
+        onRowClick={function ka() { }}
+        onRowExpandChange={function ka() { }}
+        onRowSelect={function ka() { }}
+        onSort={function ka() { }}
+        onTableScroll={function ka() { }}
+        selectedRowIds={{
+          '3': true
+        }}
+        selectionMode="SingleSelect"
+        tableHooks={[]}
+        withRowHighlight
+      />
     </>
   );
 }
