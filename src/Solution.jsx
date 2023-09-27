c50c864e6f85ff5fa4c7bf648502a6dae25e578f
Jeroen Roels <roels_jeroen@hotmail.com>
2023-09-27 07:08:57 +0000

    Week1: create a table

--- src/Solution.jsx
+++ src/Solution.jsx
@@ -1,24 +1,25 @@
 import { Label } from "@ui5/webcomponents-react";
+import { Table, TableRow, TableColumn, TableCell } from "@ui5/webcomponents-react";
 
 const tableData = [
   {
     id: 1,
-    title: "<Update Ticket Title>",
+    title: "Create table",
     status: "Completed",
   },
   {
     id: 2,
-    title: "<Update Ticket Title>",
+    title: "Load table with data",
     status: "In Progress",
   },
   {
     id: 3,
-    title: "<Update Ticket Title>",
+    title: "Use custom status component",
     status: "Not Started",
   },
   {
     id: 4,
-    title: "<Update Ticket Title>",
+    title: "Create table search",
     status: "Blocked",
   },
 ];
@@ -27,7 +28,44 @@ function Solution() {
   return (
     <>
       <Label>Display table below</Label>
-      {/* Put table component here */}
+      <Table
+        columns={
+          <>
+            <TableColumn style={{ width: '12rem' }}>
+              <Label>ID</Label>
+            </TableColumn>
+            <TableColumn minWidth={800} popinText="Supplier">
+              <Label>Title</Label>
+            </TableColumn>
+            <TableColumn>
+              <Label>Status</Label>
+            </TableColumn>
+          </>
+        }
+        onRowClick={function ka() { }}
+      >
+        {
+          tableData.length && tableData.map(tableLine =>
+          (<TableRow key={tableLine.id}>
+            <TableCell>
+              <Label>
+                {tableLine.id}
+              </Label>
+            </TableCell>
+            <TableCell>
+              <Label>
+                {tableLine.title}
+              </Label>
+            </TableCell>
+            <TableCell>
+              <Label>
+                {tableLine.status}
+              </Label>
+            </TableCell>
+          </TableRow>)
+          )
+        }
+      </Table>
     </>
   );
 }
